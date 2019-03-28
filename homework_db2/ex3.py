79794a48725013b2abe1572b5e1741c7d0061263
nam11993 <47785659+nam11993@users.noreply.github.com>
2019-03-28 15:41:16 +0700

    Update ex3.py

--- homework_db2/ex3.py
+++ homework_db2/ex3.py
@@ -1,13 +1,10 @@
 from pymongo import MongoClient
 from matplotlib import pyplot
 
-
 mongo_uri="mongodb://admin:admin@ds021182.mlab.com:21182/c4e"
 client=MongoClient(mongo_uri)
 db=client.c4e
-
 customers = db.customers.find()
-
 
 events = 0
 ads = 0
