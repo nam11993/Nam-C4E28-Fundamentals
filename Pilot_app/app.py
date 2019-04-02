80d24eccfb2c596d6e38186dc193e2a6bd32f781
nam11993 <47785659+nam11993@users.noreply.github.com>
2019-04-02 08:08:39 +0700

    Add files via upload

--- Pilot_app/app.py
+++ Pilot_app/app.py
@@ -1,11 +1,104 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, session
+from services import services
+from bson.objectid import ObjectId
+
+
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "fj39#@32F><@#.f3fe:#$P3"
 
 
 @app.route('/')
 def index():
     return render_template('homepage.html')
 
+@app.route('/all-service')
+def all_service():
+    if session["logged"]:
+        service = services.find()
+        return render_template('all_service.html', service = service)
+    else:
+        return redirect("/login")
+
+# @app.route('/all-service/male')
+# def male():
+#     male = services.find({"gender": "male"})
+#     return render_template('male.html', male = male)
+
+# @app.route('/all-service/female')
+# def female():
+#     female = services.find({"gender": "female"})
+#     return render_template('female.html', female = female)
+
+@app.route('/all-service/<gender>')
+def male_service(gender):
+    service = services.find({"gender": gender})
+    return render_template('all_service.html', service = service)
+
+@app.route('/all-service/detail/<id>/')
+def details(id):
+    detail_service = services.find_one({"_id": ObjectId(id)})
+    return render_template("detail_service.html", detail_service = detail_service)
+    
+@app.route("/delete/<id>")
+def delete(id):
+    delete_service = services.find_one({"_id": ObjectId(id)})
+    if delete_service is not None:
+        services.delete_one(delete_service)
+    else:
+        return "Not found service"
+    return redirect("/all-service")
+
+@app.route("/update/<id>", methods = ["GET","POST"])
+def update(id):
+    edit_service = services.find_one({"_id": ObjectId(id)})
+    if request.method == "GET":
+        return render_template("update_service.html", edit_service = edit_service)
+    elif request.method == "POST":
+        form = request.form
+        name = form["full_name"]
+        age = form["age"]
+        address = form["address"]
+        gender = form["gender"]
+        available = form["available"]
+        new_value = { "$set": { 
+            "name": name,
+            "age": age,
+            "address": address,
+            "gender": gender,
+            "available": available,
+        }}
+        services.update_one(edit_service, new_value)
+        return redirect("/all-service/detail/{0}/".format(id))
+
+
+@app.route("/login", methods = ["GET","POST"])
+def login():
+    if not session["logged"]: #chua dang nhap
+        if request.method == "GET":
+            return render_template("login.html")
+        elif request.method == "POST":
+            form = request.form
+            username = form["username"]
+            password = form["password"]
+            if username == "adminc4e" and password == "adminc4e":
+                session["logged"] = True
+                return redirect("/all-service")
+            else:
+                return redirect("/login")
+    else: #dang nhap roi
+        return redirect("/all-service")
+
+
+@app.route("/logout")
+def logout():
+    del session["logged"]  #c1
+    session["logged"] = False #c2
+    return redirect("/login")
+
+
+
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
- 
--- Pilot_app/services.py
+++ Pilot_app/services.py
@@ -1,16 +1,57 @@
-from model import services
+from pymongo import MongoClient
 from faker import Faker
 from random import randint, choice
+from bson.objectid import ObjectId
+
 faker = Faker()
 
-for i in range(50):
-    new_service = {
-        "name" : faker.name(),
-        "address": faker.address(),
-        "age": randint(18, 30),
-        "gender": choice(["male","female"]),
-        "height": randint(150,190),
-    }
+mongo_uri = "mongodb+srv://admin:admin@c4e28-s3lzu.mongodb.net/test?retryWrites=true"
 
-    services.insert_one(new_service)
-    print("Saved document {0}...".format(i+1))
+#1 Connect to cluster
+client = MongoClient(mongo_uri)
+
+#2 Get/ Create Database
+service_database = client.db_service
+
+#3 Get/ Creat collection
+services = service_database["services"]
+
+#4 Create document, insert document
+# for i in range(50):
+#     new_service = {
+#         "name": faker.name(),
+#         "age" : randint(18,30),
+#         "address" : faker.address(),
+#         "gender" : choice(["male", "female"]),
+#         "available" : choice([True, False]),
+#     }
+#     services.insert_one(new_service)
+#     print("Saving document {0} .... ".format(i+1))
+
+#5 Read 
+#5.1 Read all
+# all_services = services.find() #lazy loading
+# print(all_services) #same list
+# print(all_services[0])
+# for service in all_services:
+#     print(service["name"])
+
+#5.2 Read only one
+# one_service = services.find_one({"name" : "Justin Bird"})
+# one_service = services.find_one({"_id": ObjectId('5c9a1d75bc2d4c3bace02136') })
+# print(one_service)
+
+#6 delete
+# service = services.find_one({ "_id": ObjectId('5c9a1d75bc2d4c3bace02136') })
+# if service is not None:
+#     services.delete_one(service)
+# else:
+    # print("Not found document!")
+# print(service)
+
+
+#7 Update
+one_service = services.find_one({ "_id": ObjectId('5c9a1d76bc2d4c3bace0213a') })
+new_value = { "$set": { "gender": "female"} }
+services.update_one(one_service, new_value)
+print(one_service)
